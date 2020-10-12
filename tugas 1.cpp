d15fc10bf5e217a536418e58e98a58eaf56261f8
aditmulyatama <adit.mlytm21@gmail.com>
2020-10-12 15:36:09 +0700

    Tugas 1 Prak Algo

---
+++ tugas 1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string nama,nim;
+    float uts,uas;
+    double avg;
+
+    cout << "Nama      : ";
+    getline(cin,nama);
+
+    cout<< "NIM       : ";
+    cin>>nim;
+
+    cout<< "Nilai UTS : ";
+    cin>>uts;
+
+    cout<<"Nilai UAS : ";
+    cin>>uas;
+
+    avg=(uts+uas)/2;
+
+    cout<<"\n\nNama anda "<<nama<<". NIM " <<nim<<endl<<endl;
+    cout<<"Nilai akhir anda: "<<avg<<endl;
+    }
